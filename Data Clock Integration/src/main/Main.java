4d55b9849037f4ccc9de06e0a299b2cf5ac79150
Enrico <jerome.requioma@gmail.com>
2017-07-14 10:23:09 +0800

    started

--- Data Clock Integration/src/main/Main.java
+++ Data Clock Integration/src/main/Main.java
@@ -1,18 +1,21 @@
-package application;
+package main;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/theme/default.css").toExternalForm());
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Start");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
